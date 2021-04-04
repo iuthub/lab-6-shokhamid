30ce7a75f3ecb89851b4bd996ff47f35b08b24f5
shokhamid <1810139si@gmail.com>
2021-04-04 23:55:42 +0500

    lab6

--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -1,76 +1,30 @@
-<?php
-
-	$pattern="";
-	$text="";
-	$replaceText="";
-	$replacedText="";
-	$answer="";
-	$m="";
-	$match="Not checked yet.";
-
-if ($_SERVER["REQUEST_METHOD"]=="POST") {
-	$pattern=$_POST["pattern"];
-	$text=$_POST["text"];
-	$replaceText=$_POST["replaceText"];
-
-	$replacedText=preg_replace($pattern, $replaceText, $text);
-
-	if(preg_match($pattern, $text)) {
-						$match="Match!";
-					} else {
-						$match="Does not match!";
-					}
-	if(preg_match("/quick/", $text)) {
-		$answer = "Contains quick";
-	}
-
-	if(preg_match("/\+998-\d\d-\d\d\d-\d\d\d\d/", $text)) {
-		$answer ="Contains phone number";
-	}
-	$replaced = str_replace(' ', '', $text);
-	$remove = preg_replace("/[^0-9,.]/", "", $text );
-	$removeNewLine = preg_replace("/[\n]/", "", $text );
-	preg_match("/\[+[ A-z]+\]/", $text, $m);
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Validating Forms</title>
+		<link href="style.css" type="text/css" rel="stylesheet" />
+	</head>
 	
-}
-
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Valid Form</title>
-</head>
-<body>
-	<form action="regex_valid_form.php" method="post">
+	<body>
+		<h1>Registration Form</h1>
+		<p>
+			This form validates user input and then displays "Thank You" page.
+		</p>
+		
+		<hr />
+		
+		<h2>Please, fill below fields correctly</h2>
 		<dl>
-			<dt>Pattern</dt>
-			<dd><input type="text" name="pattern" value="<?= $pattern ?>"></dd>
-
-			<dt>Text</dt>
-			<dd><input type="text" name="text" value="<?= $text ?>"></dd>
-
-			<dt>Replace Text</dt>
-			<dd><input type="text" name="replaceText" value="<?= $replaceText ?>"></dd>
-
-			<dt>Output Text</dt>
-			<dd><?=	$match ?></dd>
-			<dd><?=	$answer ?></dd>
-			<dd><?=	$replaced ?></dd>
-			<dd><?=	$remove ?></dd>
-			<dd><?=	$removeNewLine ?></dd>
-			<dd><?=	implode(',', $m) ?></dd>
-
-			<dt>Replaced Text</dt>
-			<dd> <code><?=	$replacedText ?></code></dd>
-
-			<dt>&nbsp;</dt>
-			<dd><input type="submit" value="Check"></dd>
+			<dt>Name</dt>
+			<dd>
+				???
+			</dd>
+			
+			<!-- Write other fiels similar to Name as specified in lab6.pdf -->
 		</dl>
-
-	</form>
-</body>
+		
+		<div>
+			Register
+		</div>
+	</body>
 </html>
